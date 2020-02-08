6b59fe4ba629954565f041b466a4838084dd4060
Oana Ignat <oignat@umich.edu>
2020-02-08 14:17:46 -0500

    delete map and reduce classes

--- src/main/java/com/eecs476/kmeans.java
+++ src/main/java/com/eecs476/kmeans.java
@@ -1,31 +1,6 @@
-package com.eecs476
-
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+package com.eecs476;
 
 public class kmeans {
-
-    // template arguments are <input key, input value, output key, output value>
-    public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-
-        public void map(LongWritable key, Text text, Mapper.Context context) {
-            // TODO: this is a sample declaration fo a mapper
-            // you will need to implement this and add any additional mappers
-
-        }
-    }
-
-    // template arguments are <input key, input value, output key, output value>
-    public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) {
-            // TODO: this is a sample declaration fo a reducer
-            // you will need to implement this and add any additional reducers
-        }
-    }
 
 
     public static void main(String[] args) {
